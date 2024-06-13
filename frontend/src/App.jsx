ea8e48b7db8141a7507c79316d1b74e953a8863f
Austin Senft <austinsenft@gmail.com>
2024-06-13 16:40:12 -0600

    contact form rendered on contacts page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,24 @@
 import { useState, useEffect } from 'react'
 import ContactList from './ContactList'
 import './App.css'
+import ContactForm from "./ContactForm";
 
 function App() {
   const [contacts, setContacts] = useState([])
 
   useEffect(() => {
     fetchContacts()
-  }, [])
+  }, []);
 
   const fetchContacts = async () => {
     const response = await fetch("http://127.0.0.1:5000/contacts")
     const date = await response.json()
     setContacts(data.contacts)
     console.log(data.contacts)
-  }
+  };
 
-  return <ContactList contacts={contacts}/>
+  return <><ContactList contacts={contacts} />
+    <ContactForm /></>
 }
 
 export default App
