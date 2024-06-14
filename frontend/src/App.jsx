44e10c3ad3794b3ab4edab49027e2da2dadff034
Austin Senft <austinsenft@gmail.com>
2024-06-14 07:39:48 -0600

    popup modal window for entering new contact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import ContactForm from "./ContactForm";
 
 function App() {
   const [contacts, setContacts] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   useEffect(() => {
     fetchContacts()
@@ -14,11 +15,29 @@ function App() {
     const response = await fetch("http://127.0.0.1:5000/contacts")
     const data = await response.json()
     setContacts(data.contacts)
-    console.log(data.contacts)
   };
 
-  return ( <><ContactList contacts={contacts} />
-    <ContactForm /></>
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+
+  return (
+    <>
+    <ContactList contacts={contacts} />
+    <button onClick={openCreateModal}>Create New Contact</button>
+    { isModalOpen && <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={closeModal}>&times;</span>
+        <ContactForm />
+      </div>
+      </div>
+      }
+      
+    </>
   )
 }
 
